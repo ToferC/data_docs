d0ae5f376fc2dfab130388aad50332b58af3ffe9
toferc <cgeist7@gmail.com>
2022-04-21 17:02:57 -0400

    add more implementations for metadata

--- src/models/metadata.rs
+++ src/models/metadata.rs
@@ -17,9 +17,70 @@ use crate::models::{InsertableText, Text, TemplateSection,
 pub struct MetaData {
     id: Uuid,
     document_id: Uuid,
+    author_id: Uuid,
     subject: Uuid,
     category: Uuid,
+    searchable_text_en: String,
+    searchable_text_fr: String,
+    keyword_ids: Vec<Uuid>,
+    created_at: NaiveDateTime,
+    updated_at: NaiveDateTime,
+}
+
+#[derive(Debug, Serialize, Deserialize, Insertable)]
+#[table_name = "metadata"]
+pub struct InsertableMetaData {
+    document_id: Uuid,
+    author_id: Uuid,
+    subject: Uuid,
+    category: Uuid,
+    searchable_text_en: String,
+    searchable_text_fr: String,
     keyword_ids: Vec<Uuid>
+}
+
+impl MetaData {
+    pub fn create(
+        md: &InsertableMetaData,
+    ) -> Result<Self, CustomError> {
+
+        let conn = database::connection()?;
+
+        let v = diesel::insert_into(metadata::table)
+            .values(md)
+            .get_result(&conn)?;
+
+        Ok(v)
+    }
+
+    pub fn get_by_id(id: Uuid) -> Result<MetaData, CustomError> {
+        let conn = database::connection()?;
+
+        let document = metadata::table
+            .filter(metadata::id.eq(id))
+            .first::<Self>(&conn)?;
+
+        Ok(document)
+    }
+
+    pub fn get_by_document_id(document_id: Uuid) -> Result<MetaData, CustomError> {
+        let conn = database::connection()?;
+
+        let document = metadata::table
+            .filter(metadata::document_id.eq(document_id))
+            .first::<Self>(&conn)?;
+
+        Ok(document)
+    }
+
+    pub fn update(metadata: &MetaData) -> Result<Self, CustomError> {
+        let conn = metadata::connection()?;
+        let v = diesel::update(metadata::table)
+            .filter(metadata::id.eq(metadata.id))
+            .set(metadata)
+            .get_result(&conn)?;
+        Ok(v)
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, AsChangeset, Queryable, Insertable, Identifiable, Associations, PartialEq, Clone)]
